69dd3fa502deb2e3a1e67b2e668b446c04179308
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-04-24 00:28:45 +0300

    Update install-bazel.sh

--- install-bazel.sh
+++ install-bazel.sh
@@ -1,9 +1,6 @@
 set -x
 
-if [[ "${ARCH}" != "arm64" ]]; then
-    ARCH="x86_64"
-fi
-
+ARCH="x86_64"
 BAZEL_VERSION=7.6.1
 BAZEL_EXEC="/usr/bin/bazel"
 wget --no-verbose "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-linux-"${ARCH} -O ${BAZEL_EXEC}
